4cd5a252bc8bd1ea47def8436ade60b81d1bb140
Manikandan <manikandan.ramesh@xische.com>
2024-03-29 18:17:53 +0530

    changes done

--- src/ArticleDetails.js
+++ src/ArticleDetails.js
@@ -3,7 +3,6 @@ import React from "react";
 const ArticleDetails = ({ article }) => {
   return (
     <div>
-      {/*  <h2>{article.title}</h2> */}
       <p>{article.abstract}</p>
       <a href={article.url} target="_blank">
         Read More &gt;&gt;
